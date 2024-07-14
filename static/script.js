f19b7274707a17d8800fc9ec0804ee51eb4a0b0b
egor4chan <93943877+egor4chan@users.noreply.github.com>
2024-07-15 01:22:04 +0400

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,39 @@
+var health = 10;
+var shop_btn = document.getElementById('shopbtn');
+
+let tg = window.Telegram.WebApp
+
+function randint(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand)
+}
+
+function win() {
+    health -= 1
+    if (health == 0) {
+    var shop_btn = document.getElementById('shopbtn');
+    var egg = document.getElementById('clicker');
+    let label = document.getElementById('label');
+    let prelabel = document.getElementById('prelabel');
+    egg.onclick = 'cont()';
+  
+    shop_btn.style = 'opacity: 1;';
+    egg.src = 'static/success.PNG';
+    label.innerHTML = String(randint(5, 50) / 100) + ' TON'
+    prelabel.innerHTML = 'О да, в яйке оказалась бабло! Получяй бесплатные яйки за приглашения.';
+    }
+}
+
+function cont() {
+    let egg = document.getElementById('clicker');
+    let label = document.getElementById('label');
+    let prelabel = document.getElementById('prelabel');
+
+    egg.className == 'success'
+    egg.src = 'static/egg.PNG';
+    label.innerHTML = 'Яйко нищеты'
+    prelabel.innerHTML = 'Тибе врятли чота выпадет с бесплатного яйка такчто купи богатае не позорся.'
+
+
+}
+
